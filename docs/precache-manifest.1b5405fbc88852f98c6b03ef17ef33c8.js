9839b164f122536427a14ee57ed2783ba2a4277b
Israel Bravo <israelmcbravo@gmail.com>
2020-08-02 16:12:30 -0500

    docs agregado

---
+++ docs/precache-manifest.1b5405fbc88852f98c6b03ef17ef33c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5def06a864193cbbfe959f4de94929d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da21def017b502707e23",
+    "url": "/static/css/main.1d728f51.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da21def017b502707e23",
+    "url": "/static/js/main.0fd30e7a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
